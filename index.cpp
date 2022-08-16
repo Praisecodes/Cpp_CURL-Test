df75a46a0f944a0e869fdeaafa4b3090d3db4ae3
Praisecodes <cmancoder@gmail.com>
2022-08-16 17:37:48 +0100

    Finally Works!!

--- index.cpp
+++ index.cpp
@@ -1,44 +1,58 @@
 #define CURL_STATICLIB
 #include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <map>
+#include <vector>
 #include "curl/curl.h"
+
+using json = nlohmann::json;
 
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
 
-int main()
-{
-    CURL* curl;
-    CURLcode response;
-    std::string result;
-    std::string fields = "{\"username\": \"raisecodes\}";
+int main() {
+	CURL* curl;
+	CURLcode response;
+	std::string result;
+	
 
-    struct curl_slist *hs = NULL;
+	std::string details = "{\"username\": \"Praisecodes\", \"password\": \"Praise@042\"}";
 
-    //curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
+	json json_details = json::parse(details);
+	curl = curl_easy_init();
+	if (curl) {
+		struct curl_slist* headers = NULL;
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+		headers = curl_slist_append(headers, "Cache-Control: max-age=0, no-cache");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		curl_easy_setopt(curl, CURLOPT_URL, "https://practiceapii.herokuapp.com/login.php");
+		curl_easy_setopt(curl, CURLOPT_POST, 1);
+		curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, json_details.dump().c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
 
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://practiceapii.herokuapp.com/login.php");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        hs = curl_slist_append(hs, "Content-Type: application/json");
-        //hs = curl_slist_append(hs, "Cache-Control: no-cache, must-revalidate, max-age=0");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hs);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+		response = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		if (response != CURLE_OK) {
+			std::cout << "Failed Request: " << curl_easy_strerror(response) << std::endl;
+		}
+		else {
+			json json_result = json::parse(result);
 
-        response = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        if (response != CURLE_OK) {
-            std::cerr << "Request Failed: " << curl_easy_strerror(response) << std::endl;
-        }
-        else {
-            std::cout << result << fields << std::endl;
-        }
-    }
-    return 0;
+			std::vector<std::string> resultArr;
+			resultArr = json_result;
+			std::cout << resultArr[0] << std::endl;
+		}
+	}
+	else {
+		std::cerr << "Error Initialize cURL\n";
+	}
+
+	return 0;
 }
 
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return (size * nmemb);
-}
+	((std::string*)userp)->append((char*)contents, size * nmemb);
 
+	return size * nmemb;
+}
